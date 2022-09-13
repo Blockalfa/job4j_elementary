3ddea018d00117fc022bd93a0c51296bd13ade88
User <a.abidov278@gmail.com>
2022-09-13 14:43:22 +0500

    4.2. Заголовок в файле java

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args){
+        System.out.println("Abidov Alisher");
+        System.out.println("14.01.1983");
+
+    }
+}
