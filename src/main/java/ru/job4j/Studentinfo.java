c7999baadfe3f6d542ef173ee0e7b65d1d0b4d2c
User <a.abidov278@gmail.com>
2022-09-14 13:48:07 +0500

    4.2. Загаловок в файле java

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Abidov Alisher");
         System.out.println("14.01.1983");
 
